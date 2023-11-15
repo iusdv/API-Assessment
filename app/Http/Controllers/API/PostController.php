51ef886497fa84ddcba2f332b1ecbc76239b430b
iusdv <iusdevisser@gmail.com>
2023-11-15 13:46:57 +0100

    first commit, added files already did crud actions and routes

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+
+        return response()->json(['data' => $posts]);
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return response()->json(['data' => $post]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string',
+            'content' => 'required|string',
+            'author' => 'required|string',
+        ]);
+
+        $post = Post::create($validatedData);
+
+        return response()->json(['data' => $post], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string',
+            'content' => 'required|string',
+            'author' => 'required|string',
+        ]);
+
+        $post = Post::findOrFail($id);
+        $post->update($validatedData);
+
+        return response()->json(['data' => $post]);
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return response()->json(['message' => 'Post deleted successfully']);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider, and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('/home', function () {
+    return response()->json(['message' => 'Welcome to the home route']);
+});
+
+Route::prefix('posts')->group(function () {
+    Route::get('/', [PostController::class, 'index']);
+    Route::get('/{id}', [PostController::class, 'show']);
+    Route::post('/', [PostController::class, 'store']);
+    Route::put('/{id}', [PostController::class, 'update']);
+    Route::delete('/{id}', [PostController::class, 'destroy']);
+});
+
+
+
+// // Routes for UserController
+// Route::prefix('users')->group(function () {
+//     Route::get('/', [UserController::class, 'index']);
+//     Route::get('/{id}', [UserController::class, 'show']);
+//     Route::post('/', [UserController::class, 'store']);
+//     Route::put('/{id}', [UserController::class, 'update']);
+//     Route::delete('/{id}', [UserController::class, 'destroy']);
+// });
